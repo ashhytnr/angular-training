3e1bf228df7d69a4e112f6cab8c35027e0f27453
Ash Toner <ash.toner@dvsa.gov.uk>
2025-10-17 15:53:22 +0100

    teehee

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+
+  // found this code from angular tutorial
+
+
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss'
+})
+export class Hello {
+
+}
+
+
+// @Component({
+//   selector: 'app-footer',
+//   templateUrl: '/hello.html',
+//   styleUrl: '/hello.scss',
+// })
+//
+// export class Footer {
+// //   component behavior is defined here
+//   // Use new Date().getFullYear() in your TypeScript file.
+//   // Add the <app-footer> tag to app.component.html.
+// }

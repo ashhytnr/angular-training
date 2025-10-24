8c8881843fcc49c5169a10334bbe93ee906cbda8
Ash Toner <ash.toner@dvsa.gov.uk>
2025-10-24 16:54:14 +0100

    add-input

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,20 +1,48 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component ({
   selector: 'app-counter',
+  imports: [],
   templateUrl: "./counter.component.html"
 })
 
-export class CounterComponent {
+export class CounterComponent implements OnInit, OnChanges, OnDestroy{
 
-  counter = 0;
+  @Input()
+  //
+
+  counterNew: number = 0
+  // counter = 0;
+
+  @Input()
+  randomText!: string
 
   @Output()
   counterChange = new EventEmitter<number>();
 
   addToCount() {
-    this.counter ++
-    this.counterChange.emit(this.counter);
+    this.counterNew ++
+    this.counterChange.emit(this.counterNew);
+  }
 
+  functionNameMaybe() {
+    this.counterNew ++
+    console.log(this.counterNew)
+
+    if(this.counterNew > 10){
+      this.randomText = 'yayyy!!';
+    }
+  }
+
+  ngOnInit():void {
+    console.log("This has been initialised!")
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(`This is a change, ${changes}`)
+  }
+
+  ngOnDestroy() {
+    console.log("This has been destroyed")
   }
 }
